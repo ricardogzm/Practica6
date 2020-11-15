88d854ae22f45c3cc23a9af1f368bcac64ca5066
Juan Pablo <juanpablo.andrademiron@gmail.com>
2020-11-14 21:35:15 -0600

    Barbero V1.6.3

--- barbero.c
+++ barbero.c
@@ -4,22 +4,24 @@
 
 void barbero(int semaforos, int cortes, int **clEsperando) {
     FILE *fd;
-    fd= fopen("barbero.txt", "w");
     printf("antes");
     while (1){
         printf("despues");
+        fd= fopen("barbero.txt", "a");
         fprintf(fd,"El barbero esta: Dormido\n Cortes hechos: %d \n", cortes);
-
+        fclose(fd);
         manejarSemaforo(semaforos, clientes, down); //barbero durmiendo si hay 0 clientes
         manejarSemaforo(semaforos, mutex, down); //espera para adquirir acceso a sillas disponibles
 
         (**clEsperando) ++; //decrementar un cliente en la lista de espera
         cortes++; //aumenta el numero de cortes
+        fd= fopen("barbero.txt", "a");
         fprintf(fd,"El barbero esta: ocupado \n Cortes hechos: %d \n", cortes);
         sleep(5);
 
         manejarSemaforo(semaforos, barberoDisponible, up); //el barbero esta listo para cortar el pelo
         manejarSemaforo(semaforos, mutex, up);  //libera acceso a sillas disponibles
+        fclose(fd);
     }
-    fclose(fd);
+
 }
